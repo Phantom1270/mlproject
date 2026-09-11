0af1e7d6ed876b7245f4e16872977928a3ef3aa7
Ujjwal Shukla <ujjwal.shukla2509@gmail.com>
2026-09-11 17:50:38 +0530

    removed catboost and xgboost as they are causing errors while deployment

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -43,8 +43,6 @@ class ModelTrainer:
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
                 "K-Neighbors Regressor": KNeighborsRegressor(),
-                "XGBRegressor": XGBRegressor(),
-                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
             params={
@@ -68,15 +66,6 @@ class ModelTrainer:
                                 'n_estimators': [8,16,32,64,128,256]
                             },
                             "Linear Regression":{},
-                            "XGBRegressor":{
-                                'learning_rate':[.1,.01,.05,.001],
-                                'n_estimators': [8,16,32,64,128,256]
-                            },
-                            "CatBoosting Regressor":{
-                                'depth': [6,8,10],
-                                'learning_rate': [0.01, 0.05, 0.1],
-                                'iterations': [30, 50, 100]
-                            },
                             "AdaBoost Regressor":{
                                 'learning_rate':[.1,.01,0.5,.001],
                                 'loss':['linear','square','exponential'],
