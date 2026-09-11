f856bafef813047c0d97b90ce57fd40c7eceaa39
Ujjwal Shukla <ujjwal.shukla2509@gmail.com>
2026-09-11 17:56:50 +0530

    removing a few more dependencies

--- application.py
+++ application.py
@@ -23,9 +23,9 @@ def predict_datapoint():
         data=CustomData(
             gender=request.form.get('gender'),
             race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request. form.get('parental_level_of_education'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
-            test_preparation_course=request. form.get('test_preparation_course'),
+            test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('reading_score')),
             writing_score=float(request.form.get('writing_score')),
         )
